6e8527441ecd462d49e9f7939b2ead619b85e7f6
aniknia <aniknia.dev@gmail.com>
2022-02-26 03:36:03 -0800

    fix: change pininput layout for better ux

--- src/components/CodeInput.js
+++ src/components/CodeInput.js
@@ -1,24 +1,31 @@
-import { HStack, PinInput, PinInputField } from '@chakra-ui/react'
+import { Box, HStack, PinInput, PinInputField } from '@chakra-ui/react'
 import { ArrowRightIcon } from '@primer/octicons-react'
+
+// TODO: Decide how to take in information
+// Either <PinIn /> <Arrow /> <Pinin />
+// or
+// <PinIn> <Arrow /> </Pinin>
+// The former is easier to parse but the latter is better ux
 
 export default function CodeInput(props) {
     return (
-        <div name='route'>
-            <HStack>
-                <PinInput type='alphanumeric' name='start'>
-                    <PinInputField />
-                    <PinInputField />
-                    <PinInputField />
-                    <PinInputField />
-                </PinInput>
-                <ArrowRightIcon size={24} />
-                <PinInput type='alphanumeric' name='end'>
-                    <PinInputField />
-                    <PinInputField />
-                    <PinInputField />
-                    <PinInputField />
-                </PinInput>
-            </HStack>
-        </div>
+        <Box>
+            <p>Enter your airport codes below.</p>
+            <Box maxW='sm' borderWidth='1px' borderRadius='lg' p={2} name='route'>
+                <HStack>
+                    <PinInput type='alphanumeric' name='start'>
+                        <PinInputField />
+                        <PinInputField />
+                        <PinInputField />
+                        <PinInputField />
+                        <ArrowRightIcon size={24} />
+                        <PinInputField />
+                        <PinInputField />
+                        <PinInputField />
+                        <PinInputField />
+                    </PinInput>
+                </HStack>
+            </Box>
+        </Box>
     )
 }
