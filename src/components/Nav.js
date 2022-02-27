3b2ef5a431036ac383fb8d638cecb4c7354aed59
aniknia <aniknia.dev@gmail.com>
2022-02-26 16:58:00 -0800

    feat: change navigation structure

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,30 @@
+import { Drawer, DrawerContent, DrawerHeader, DrawerBody, DrawerFooter, Stack, Center } from '@chakra-ui/react'
 import NavHead from './NavHead'
 import CodeInput from './CodeInput'
 import FlightList from './FlightList'
 import Credit from './Credit'
 
+// TODO: Fix DrawerFooter not centering with out Center being the outside tag
+
 export default function Nav() {
     return (
-       <nav>
-           <NavHead />
-           <CodeInput />
-           <FlightList />
-           <Credit />
-       </nav> 
+       <Drawer isOpen={true} placement='left' size='sm'>
+           <DrawerContent>
+               <DrawerHeader>
+                   <NavHead />
+               </DrawerHeader>
+               <DrawerBody>
+                   <Stack spacing='24px'>
+                       <CodeInput />
+                       <FlightList />
+                   </Stack>
+               </DrawerBody>
+               <Center>
+                    <DrawerFooter borderTopWidth='2px'>
+                       <Credit />
+                    </DrawerFooter>
+               </Center>
+           </DrawerContent>
+       </Drawer> 
     )
 }
