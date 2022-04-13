e616d5a3a4ad7982f2198d9f61d6ecde2c600752
aniknia <aniknia.dev@gmail.com>
2022-04-13 03:12:42 -0700

    feat: add visual feedback for incorrect codes for code inputs

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -4,28 +4,35 @@ import { useState, useEffect } from 'react'
 // TODO: Extend PinInput theme so that when the airport does not exist a red highlight appears
 
 export default function InputField(props) {
-    const [value, setValue] = useState();
+    const [value, setValue] = useState("");
+    const [error, setError] = useState(false);
 
     useEffect(() => {
-        fetch("http://localhost:5000/v1/check_code/" + value)
-        .then(response => response.json())
-        .then(data => {
-            if (data["default"] === true) {
-                props.flag(false);
-                console.log("empty")
-            } else if (data["check_code"] === true) {
-                props.set(value);
-                props.flag(true);
-                console.log("airport exists");
-            } else {
-                props.flag(false);
-                console.log("airport does not exist");
+        if(value.length >= 3) {
+            fetch("http://localhost:5000/v1/check_code/" + value)
+            .then(response => response.json())
+            .then(data => {
+                if (data["default"] === true) {
+                    props.flag(false);
+                    setError(false);
+                    console.log("empty")
+                } else if (data["check_code"] === true) {
+                    props.set(value);
+                    props.flag(true);
+                    setError(false);
+                    console.log("airport exists");
+                } else {
+                    props.flag(false);
+                    setError(true);
+                    console.log("airport does not exist");
+                }
+            })
             }
-        })
-    });
+        }
+    );
 
     return (
-        <PinInput type='alphanumeric' onChange={setValue}>
+        <PinInput type='alphanumeric' isInvalid={error} onChange={setValue}>
             <PinInputField />
             <PinInputField />
             <PinInputField />
