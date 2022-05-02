d508921d0876da4713d3091505b7231db1771491
aniknia <aniknia.dev@gmail.com>
2022-05-02 00:04:57 -0700

    feat: add title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useEffect} from "react";
 import {
   ChakraProvider,
   theme
 } from '@chakra-ui/react';
 import Main from './components/Main'
 
+
+
 function App() {
+  useEffect(() => {
+     document.title = "The Flight Mapper";  
+   }, []);
   return (
     <ChakraProvider theme={theme}>
       <Main />
