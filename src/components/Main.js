cef64b806bd45dba02ffdb1019af76a1bfc4abf9
aniknia <aniknia.dev@gmail.com>
2022-05-14 14:20:56 -0700

    chore: add todo about favicon

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,8 @@ import Map from './Map'
 import Nav from './Nav'
 import RoutesProvider from './RoutesProvider'
 
+// TODO: make favicon blue for visibility on light and dark mode browsers
+
 export default function Main() {
     return (
         <div>
