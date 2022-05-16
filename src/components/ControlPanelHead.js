527f4e80dbf20750efb10d12b8e9745b0b48626e
aniknia <aniknia.dev@gmail.com>
2022-05-15 19:09:45 -0700

    refactor: add logo component to controlpanelhead

--- src/components/ControlPanelHead.js
+++ src/components/ControlPanelHead.js
@@ -1,19 +1,13 @@
 import { IoAirplane } from '@react-icons/all-files/io5/IoAirplane'
-import { Flex, Spacer, HStack, CloseButton } from '@chakra-ui/react'
-import { ColorModeSwitcher } from '../ColorModeSwitcher'
+import { Flex, Spacer, CloseButton } from '@chakra-ui/react'
+import Logo from './Logo'
 
 export default function ControlPanelHead(props) {
     return (
         <Flex>
-            <HStack>
-                <IoAirplane />
-                <p>The Flight Mapper</p>
-            </HStack>
+            <Logo />
             <Spacer />
-            <HStack>
-                <ColorModeSwitcher />
-                <CloseButton size='md' onClick={props.close} />
-            </HStack>
+            <CloseButton size='md' onClick={props.close} />
         </Flex>
     )
 }
