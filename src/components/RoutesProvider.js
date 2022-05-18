0e342eacd6d629a110ce85062f23d98f810da508
aniknia <aniknia.dev@gmail.com>
2022-05-17 17:00:17 -0700

    refactor: simplify api call

--- src/components/RoutesProvider.js
+++ src/components/RoutesProvider.js
@@ -20,9 +20,23 @@ export default function RoutesProvider(props) {
         })
         if (existsFlag === false) {
             if (error === false) {
-                route = unify_route(route);
-                setRoutes([...routes, route]);
-                console.log("route was added to provider")
+                fetch(process.env.REACT_APP_API_HOST + "/v1/get-route/" + route.start + "/" + route.end)
+                .then(response => response.json())
+                .then(data => {
+                    if (data["error"] === true) {
+                        setError(true);
+                        console.log("something went wrong");
+                    } else {
+                        setError(false);
+                        route.x = data["x"];
+                        route.y = data["y"];
+                        route.time = data["time"];
+                        route.distance = data["distance"];
+                        route.carbon = data["carbon"];
+                        setRoutes([...routes, route])
+                        console.log("route was added to provider")
+                    }
+                })
             } else {
                 console.log("something went wrong")
             }
@@ -55,75 +69,6 @@ export default function RoutesProvider(props) {
         }
     }
 
-    function unify_route(route) {
-        route = find_coordinates(route);
-        route = find_time(route);
-        route = find_distance(route);
-        route = find_carbon(route);
-        return route
-    }
-
-    function find_coordinates(route) {
-        fetch(process.env.REACT_APP_API_HOST + "/v1/get-route/" + route.start + "/" + route.end)
-            .then(response => response.json())
-            .then(data => {
-                if (data["error"] === true) {
-                    setError(true);
-                    console.log("something went wrong");
-                } else {
-                    setError(false);
-                    route.x = data["x"];
-                    route.y = data["y"]
-                    console.log("coordinates were saved");
-                }
-            })
-        return route;
-    }
-    function find_time(route) {
-        fetch(process.env.REACT_APP_API_HOST + "/v1/get-time/" + route.start + "/" + route.end)
-            .then(response => response.json())
-            .then(data => {
-                if (data["error"] === true) {
-                    setError(true);
-                    console.log("something went wrong");
-                } else {
-                    setError(false);
-                    route.time = data["time"];
-                    console.log("time was saved");
-                }
-            })
-        return route;
-    }
-    function find_distance(route) {
-        fetch(process.env.REACT_APP_API_HOST + "/v1/get-distance/" + route.start + "/" + route.end)
-            .then(response => response.json())
-            .then(data => {
-                if (data["error"] === true) {
-                    setError(true);
-                    console.log("something went wrong");
-                } else {
-                    setError(false);
-                    route.distance = data["distance"];
-                    console.log("distance was saved");
-                }
-            })
-        return route;
-    }
-    function find_carbon(route) {
-        fetch(process.env.REACT_APP_API_HOST + "/v1/get-carbon/" + route.start + "/" + route.end)
-            .then(response => response.json())
-            .then(data => {
-                if (data["error"] === true) {
-                    setError(true);
-                    console.log("something went wrong");
-                } else {
-                    setError(false);
-                    route.carbon = data["carbon"];
-                    console.log("carbon footprint was saved");
-                }
-            })
-        return route;
-    }
     return (
         <Routes.Provider value={{routes: routes, addRoute: addRoute, removeRoute: removeRoute}}>
             {props.children}
