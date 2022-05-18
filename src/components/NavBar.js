676947c48f05d0ec588d2db1a5a9d0fb053adb44
aniknia <aniknia.dev@gmail.com>
2022-05-17 17:51:24 -0700

    chore: add some text about what the time and carbon footprint values are estimated from

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,7 +18,7 @@ export default function NavBar() {
         </Box>
         <Spacer />
         <Box>
-          <NavModal button={<QuestionIcon size={24} />} title="Info" body= "This app allows you to map out flights between airports." />
+          <NavModal button={<QuestionIcon size={24} />} title="Info" body= "This app allows you to map out flights between airports. The time and corbon foot print for each flight are estimated using a Boeing 747." />
           <ColorModeSwitcher />
         </Box>
       </Flex>
