4230e3fb5ba031f31e6b55f18b90b797ed3806c2
ari <aniknia.dev@gmail.com>
2022-06-01 22:31:03 -0700

    style: change navy blue to light blue

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,21 +1,32 @@
-import { useContext } from "react"
-import useWindowDimensions from "./useWindowDimension"
-import { Flex, useColorModeValue } from "@chakra-ui/react"
-import Map from "./Map"
-import { Routes } from "./RoutesProvider"
+import { useContext } from 'react';
+import useWindowDimensions from './useWindowDimension';
+import { Flex, useColorModeValue } from '@chakra-ui/react';
+import Map from './Map';
+import { Routes } from './RoutesProvider';
 
 export default function Window() {
-    const value = useContext(Routes)
-    const color = useColorModeValue('blue', 'orange')
-    const image = useColorModeValue("map_light.svg", "map_dark.svg")
-    const { height, width } = useWindowDimensions()
+  const value = useContext(Routes);
+  const color = useColorModeValue('#45b6fe', 'orange');
+  const image = useColorModeValue('map_light.svg', 'map_dark.svg');
+  const { height, width } = useWindowDimensions();
 
-    const final_height = height - 57
-    const final_width = final_height * (1.73)
+  const final_height = height - 57;
+  const final_width = final_height * 1.73;
 
-    return (
-        <Flex width={width} height={final_height} align="center" justifyContent='center'>
-            <Map value={value} width={final_width} height={final_height} color={color} image={image} />
-        </Flex>
-    )
+  return (
+    <Flex
+      width={width}
+      height={final_height}
+      align="center"
+      justifyContent="center"
+    >
+      <Map
+        value={value}
+        width={final_width}
+        height={final_height}
+        color={color}
+        image={image}
+      />
+    </Flex>
+  );
 }
