d66313a29d972172bce3ce1b798bf4a9c4992431
ari <aniknia.dev@gmail.com>
2022-06-01 22:48:05 -0700

    style: changed sun and moon icons

--- src/components/ColorModeSwitcher.js
+++ src/components/ColorModeSwitcher.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
-import { FaMoon, FaSun } from 'react-icons/fa';
+import { MoonIcon, SunIcon } from '@primer/octicons-react';
 
 export const ColorModeSwitcher = props => {
   const { toggleColorMode } = useColorMode();
   const text = useColorModeValue('dark', 'light');
-  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+  const SwitchIcon = useColorModeValue(MoonIcon, SunIcon);
 
   return (
     <IconButton
